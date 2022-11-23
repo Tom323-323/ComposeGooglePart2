1ba98ec2aac10ddf5a0b3c67c728572e4fab3f1d
Tom323-323 <64865370+Tom323-323@users.noreply.github.com>
2022-11-23 21:20:52 +0300

    Initial commit

---
+++ app/src/main/java/com/tomaslab/composegooglepart2/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.tomaslab.composegooglepart2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.foundation.Image
+import androidx.compose.runtime.*
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.tomaslab.composegooglepart2.ui.theme.ComposeGooglePart2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeGooglePart2Theme {
+                DiceRollerApp()
+            }
+        }
+    }
+}
+
+
+
+
+@Composable
+fun DiceRollerApp(){
+    DiceWithButtonAndImage(modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center)
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+
+    var result by remember{ mutableStateOf(1) }
+
+    val imageResourse = when(result){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+        else -> {R.drawable.dice_6}
+    }
+
+    Column (
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+
+        Image(painter = painterResource(imageResourse), contentDescription = result.toString())
+
+        Spacer (modifier = Modifier.height(16.dp))
+
+        Button(onClick = { result = (1..6).random() }) {
+               Text(stringResource(R.string.roll)
+               )
+        }
+    }
+}
+
+
